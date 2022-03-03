78b116c056fc4ca2c5da93cdc928b8992af4cc77
Robin Jones <networkfusion@users.noreply.github.com>
2022-03-03 18:49:36 +0000

    Add further debug.

--- install-nf-build-components.ps1
+++ install-nf-build-components.ps1
@@ -8,6 +8,7 @@ function DownloadVsixFile($fileUrl, $downloadFileName)
 }
 
 $tempDir = $($Env:RUNNER_TEMP)
+Write-Host "TempDir is: $tempDir"
 
 # Get nanoFramework VS extension information from Open VSIX Gallery feed
 $vsixFeedXml = Join-Path $tempDir "vs-extension-feed.xml"
@@ -19,66 +20,70 @@ for ($i = 0; $i -lt $feedDetails.feed.entry.Count; $i++) {
 
     if($feedDetails.feed.entry[$i].id -eq '455f2be5-bb07-451e-b351-a9faf3018dc9')
     {
-        Write-Host "VS2019->$i"
+#         Write-Host "VS2019->$i"
         $idVS2019 = $i
     }
     elseif($feedDetails.feed.entry[$i].id -eq 'bf694e17-fa5f-4877-9317-6d3664b2689a')
     {
-        Write-Host "VS2022->$i"
+#         Write-Host "VS2022->$i"
         $idVS2022 = $i
     }
     elseif($feedDetails.feed.entry[$i].id -eq '47973986-ed3c-4b64-ba40-a9da73b44ef7')
     {
-        Write-Host "VS2017->$i"
+#         Write-Host "VS2017->$i"
         $idVS2017 = $i
     }
     else {
-        Write-Host "?????[$i]"
+#         Write-Host "?????[$i]"
     }
 }
 
 # Find which VS version is installed
 $VsWherePath = "${env:PROGRAMFILES(X86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 
-Write-Output "VsWherePath is: $VsWherePath"
+Write-Host "VsWherePath is: $VsWherePath"
 
 $VsInstance = $(&$VSWherePath -latest -property displayName)
 
-Write-Output "Latest VS is: $VsInstance"
+Write-Host "Latest found VS instance is: $VsInstance"
 
 # Get extension details according to VS version, starting from VS2022 down to VS2017
 if($vsInstance.Contains('2022'))
 {
     $extensionUrl = $feedDetails.feed.entry[$idVS2022].content.src
-    $vsixPath = Join-Path  $tempDir "nanoFramework.Tools.VS2022.Extension.zip"
+    $vsixDownloadPath = Join-Path  $tempDir "nanoFramework.Tools.VS2022.Extension.zip"
     $extensionVersion = $feedDetails.feed.entry[$idVS2022].Vsix.Version
 }
 elseif($vsInstance.Contains('2019'))
 {
     $extensionUrl = $feedDetails.feed.entry[$idVS2019].content.src
-    $vsixPath = Join-Path  $tempDir "nanoFramework.Tools.VS2019.Extension.zip"
+    $vsixDownloadPath = Join-Path  $tempDir "nanoFramework.Tools.VS2019.Extension.zip"
     $extensionVersion = $feedDetails.feed.entry[$idVS2019].Vsix.Version
 }
 elseif($vsInstance.Contains('2017'))
 {
     $extensionUrl = $feedDetails.feed.entry[$idVS2017].content.src
-    $vsixPath = Join-Path  $tempDir "nanoFramework.Tools.VS2017.Extension.zip"
+    $vsixDownloadPath = Join-Path  $tempDir "nanoFramework.Tools.VS2017.Extension.zip"
     $extensionVersion = $feedDetails.feed.entry[$idVS2017].Vsix.Version
 }
 
 # Download VS extension
-DownloadVsixFile $extensionUrl $vsixPath
+DownloadVsixFile $extensionUrl $vsixDownloadPath
 
-# Unzip extension
-Write-Host "Unzip VS extension content"
-Expand-Archive -LiteralPath $vsixPath -DestinationPath $tempDir\nf-extension\
+# Unzip extension --- TODO: lets use 7zip here for expected results?!
+Write-Host "Unzipping nF VS extension package"
+Expand-Archive -LiteralPath $vsixDownloadPath -DestinationPath $tempDir\nf-extension
+
+$tempCheckDir = Get-ChildItem -Path "$tempDir\nf-extension"
+Write-Host $tempCheckDir
 
 # copy build files to msbuild location
 $VsPath = $(&$VsWherePath -latest -property installationPath)
 
-Write-Debug "Copy build files to msbuild location"
-
+Write-Host "Copying build files to msbuild location"
 $msbuildPath = $VsPath + "\MSBuild"
+Write-Host "tempExt Path: $tempDir\nf-extension\`$MSBuild\nanoFramework"
+Write-Host "msbuildPath: $msbuildPath"
 
 Copy-Item -Path "$tempDir\nf-extension\`$MSBuild\nanoFramework" -Destination $msbuildPath -Recurse
 
